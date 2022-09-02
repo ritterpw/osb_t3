aa2565f4a856de25ad8a44c949f9a95dcfb069ae
paul Ritter <ritterpw@dukes.jmu.edu>
2022-09-01 21:07:06 -0400

    intial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import { trpc } from "../utils/trpc";
+
+const Home: NextPage = () => {
+  const hello = trpc.useQuery(["example.hello", { text: "from tRPC" }]);
+
+  return <h1 className="text-center  text-sky-800">Hello World</h1>;
+};
+
+export default Home;
