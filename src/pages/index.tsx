a047055aa9b993db877f972a9d2b264fb1a9585a
paul Ritter <ritterpw@dukes.jmu.edu>
2022-09-03 16:55:12 -0400

    styling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -11,7 +11,6 @@ export default function Home() {
     <div>
       <div className=" h-screen w-screen flex flex-col ">
         <Header />
-
         <MostPopular />
       </div>
     </div>
@@ -21,22 +20,24 @@ export default function Home() {
 function Header() {
   return (
     <div className="header">
-      <div className="flex-shrink-0 text-3xl font-bold text-center uppercase ml-4 relative mr-4 ">
-        <h1>OSP</h1>
-      </div>
+      <div className="  inline-flex  justify-center gap-1">
+        <div className="flex-shrink-0 text-3xl font-bold text-center uppercase ml-4 relative mr-4 ">
+          <h1>OSP</h1>
+        </div>
 
-      <form
-        className="flex  items-center h-5 m-auto  w-96 space-x-2 space rounded-md border
+        <form
+          className="flex  self-center items-center h-6   w-96 space-x-2 space rounded-md border
          border-gray-700 bg-gray-600   py-2"
-      >
-        <MagnifyingGlassIcon className=" h-3 w-3 text-gray-400 " />
-        <input
-          className="flex-1 bg-transparent outline-none text-gray-400 placeholder:text-sm"
-          type="text"
-          placeholder="Search"
-        />
-        <button type="submit" hidden />
-      </form>
+        >
+          <MagnifyingGlassIcon className=" ml-2 h-4 w-4 text-gray-400 " />
+          <input
+            className="flex-1 bg-transparent outline-none text-gray-400 placeholder:text-sm"
+            type="text"
+            placeholder="Search"
+          />
+          <button type="submit" hidden />
+        </form>
+      </div>
 
       <div className=" text-xl mx-4 flex items-center cursor-pointer  ">
         <h1 className="header__link mx-4 hover:text-emerald-200">Home</h1>
--- src/pages/most_popular.tsx
+++ src/pages/most_popular.tsx
@@ -1,17 +1,19 @@
+import {
+  ArrowDownCircleIcon,
+  InformationCircleIcon,
+} from "@heroicons/react/24/outline";
 import Image from "next/image";
-
-export async function getServerSideProps() {}
 
 export default function MostPopular() {
   return (
-    <>
-      <div className=" bg-gray-800  rounded-b-xl  shadow-gray-800  drop-shadow-sm  h-96   ">
+    <div>
+      <div className="  bg-gradient-to-b  from-gray-800 to-gray-900 rounded-b-xl  shadow-gray-800  drop-shadow-sm  h-96   ">
         <div className="grid lg:grid-cols-[1.6fr_1fr] ">
           <div className=" items-center  text-center justify-center place-items-center flex">
-            <div className=" text-center">
+            <div className=" text-center lg:text-left">
               <p className="  text-7xl  ">Open Source</p>
               <p className="  text-7xl  ">Productions</p>
-              <p className=" mt-4  text-xl  text-emerald-300">
+              <p className=" mt-6  text-xl  text-emerald-300">
                 Collaborate with producers around the world
               </p>
 
@@ -30,9 +32,9 @@ export default function MostPopular() {
           </div>
         </div>
       </div>
-      <div className="lg:my-20  ">
-        <h1 className=" text-center my-5 text-4xl"> Most Popular Ideas</h1>
-        <div className=" mx-8 my-10 grid lg:grid-cols-3   gap-10 ">
+      <div className="lg:py-20  bg-gradient-to-b  from-gray-900 to-gray-700 ">
+        <h1 className=" text-center my-5 text-4xl"> Ideas Of The Week</h1>
+        <div className=" mx-8 py-10 grid lg:grid-cols-3   gap-10 ">
           <Card
             name={"some beat "}
             description={"this is some beat "}
@@ -55,7 +57,8 @@ export default function MostPopular() {
           />
         </div>
       </div>
-    </>
+      <div className=" bg-gradient-to-b from-gray-700 h-40 container to-gray-900  w-screen"></div>
+    </div>
   );
 }
 
@@ -69,20 +72,36 @@ function Card({
   tags: string[];
 }) {
   return (
-    <div className=" max-w-sm  rounded-lg overflow-hidden shadow-lg bg-gray-700  ">
+    <div className=" max-w-sm container  rounded-lg overflow-hidden shadow-lg bg-gray-800  ">
       <div className="px-6 py-4">
         <div className="font-bold text-xl mb-2">{name}</div>
         <p className="text-gray-200 text-base">{description}</p>
       </div>
-      <div className="px-6 pt-4 pb-2">
-        {tags.map((tag) => {
-          return (
-            <span className="inline-block bg-gray-600 rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2 shadow-md">
+      <div className="px-6 pt-4 pb-2  flex  justify-between">
+        <div>
+          {tags.map((tag) => (
+            <span
+              key={tag}
+              className="inline-block bg-gray-600 cursor-pointer hover:bg-gray-700 rounded-full px-3 py-1 text-sm font-semibold text-gray-200 mr-2 mb-2 shadow-md"
+            >
               {tag}
             </span>
-          );
-        })}
+          ))}
+        </div>
+
+        <div className="flex">
+          <InformationCircleIcon className=" mx-1 h-8 w-8 cursor-pointer hover:text-emerald-600  " />
+          <ArrowDownCircleIcon className=" mx-1 h-8 w-8 cursor-pointer hover:text-emerald-600" />
+        </div>
       </div>
+    </div>
+  );
+}
+
+function DetailsButton() {
+  return (
+    <div>
+      <InformationCircleIcon className="h-7 w-7 " />
     </div>
   );
 }
