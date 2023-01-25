d7cd3cf9ba0eab1fbded616f53f6339e97453b7b
paul Ritter <ritterpw@dukes.jmu.edu>
2023-01-25 00:47:47 -0500

    make prisma adapter work

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,7 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import { PrismaAdapter } from "@next-auth/prisma-adapter";
-import prisma from "../../../server/db/client";
+import { prisma } from "../../../server/db/client";
 
 // For more information on each option (and a full list of options) go to
 // https://next-auth.js.org/configuration/options
