24dcf56487fedb0d11aeeb93630b75a235b3d6a2
paul Ritter <ritterpw@dukes.jmu.edu>
2023-05-12 00:59:19 -0400

    Update [id].tsx
    update user page

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,11 +1,79 @@
 import { useRouter } from "next/router";
 import React from "react";
+import Layout from "@/components/layouts/profLayout";
+import { useSession } from "next-auth/react";
+import { trpc } from "@/utils/trpc";
+import Card from "@/components/card";
+import { getSession } from "next-auth/react";
+import { User } from "@prisma/client";
 
-function UserPage() {
-  const router = useRouter();
-  const { id } = router.query;
+function UserPage({
+  id,
+  user,
+  isMe,
+}: {
+  id: string;
+  user: User;
+  isMe: boolean;
+}) {
+  const { data: user_data, isLoading } = trpc.useQuery(
+    ["user.getUserAndIdeasById", { userId: id }],
+    {
+      refetchInterval: false,
+      refetchOnReconnect: false,
+      refetchOnWindowFocus: false,
+    }
+  );
 
-  return <div>{id}</div>;
+  if (isLoading) {
+    return <div>...loading</div>;
+  }
+
+  if (!user_data) {
+    return <div>...error</div>;
+  }
+
+  const ideas = user_data.user?.ideas;
+
+  return (
+    <Layout pageTitle="Profile">
+      <div className="  sm:h-[80vh] h-full flex  bg-vercel-900 sm:rounded-3xl sm:shadow-2xl sm:bg-gray-800 sm:bg-opacity-20  text-xl text-emerald-400">
+        <div className="overflow-y-scroll h-full w-full flex flex-col ">
+          <div className="pl-6 py-3 text-3xl border-b border-vercel-600 justify-center">
+            My Ideas
+          </div>
+          <div className=" px-5 sm:px-10 gap-10  md:grid lg:grid-cols-2 xl:grid-cols-3  overflow-y-scroll">
+            {!ideas ||
+              (ideas && ideas.length == 0 && (
+                <div className="text-xl pt-14">No Ideas</div>
+              ))}
+            {ideas &&
+              ideas.length > 0 &&
+              ideas.map((idea) => (
+                <div
+                  key={idea.file}
+                  className=" pt-7 last:pb-7 sm:pt-14 sm:last:pb-14"
+                >
+                  <Card idea={idea} />
+                </div>
+              ))}
+          </div>
+        </div>
+      </div>
+    </Layout>
+  );
 }
-
 export default UserPage;
+
+export const getServerSideProps = async (ctx: any) => {
+  const session = await getSession(ctx);
+  const id = ctx.params.id;
+
+  return {
+    props: {
+      id: id,
+      user: session?.user ?? null,
+      isMe: session?.user?.id === id,
+    },
+  };
+};
