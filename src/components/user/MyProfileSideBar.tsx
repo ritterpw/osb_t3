42e8444eac35e928dc994e1438ecf41c52a63a9c
paul Ritter <ritterpw@dukes.jmu.edu>
2023-05-12 01:00:33 -0400

    add user side bar components

---
+++ src/components/user/MyProfileSideBar.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import { Cog6ToothIcon, HomeIcon } from "@heroicons/react/24/outline";
+import { useRouter } from "next/router";
+type Props = {};
+
+const MyProfileSideBar = (props: Props) => {
+  const router = useRouter();
+  return (
+    <div>
+      <ul>
+        <li
+          className={` flex cursor-pointer items-center gap-2 py-3 text-vercel-400 hover:bg-vercel-700 ${
+            router.pathname === "/me" ? "border-l-4 border-emerald-600" : ""
+          }`}
+          onClick={() => router.push("/me")}
+        >
+          <HomeIcon className="ml-12 h-9 w-9  object-center" />
+          <h1 className=" pl-4 text-lg ">My Profile</h1>
+        </li>
+
+        <li
+          className={` flex cursor-pointer items-center gap-2 py-3 text-vercel-400 hover:bg-vercel-700 ${
+            router.pathname === "/settings"
+              ? "border-l-4 border-emerald-600"
+              : ""
+          }`}
+          onClick={() => router.push("/settings")}
+        >
+          <Cog6ToothIcon className="ml-12 h-9 w-9 object-center " />
+          <h1 className=" pl-4 text-lg ">Settings</h1>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default MyProfileSideBar;
