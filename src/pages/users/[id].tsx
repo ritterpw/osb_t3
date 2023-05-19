d088e4a1b7715a2112c8fbfb40a52efd66612f6c
paul Ritter <ritterpw@dukes.jmu.edu>
2023-05-18 20:23:16 -0400

    Update [id].tsx
    fix vercel warnings and add functionality for if the user is the logged in user or not

--- src/pages/users/[id].tsx
+++ src/pages/users/[id].tsx
@@ -1,7 +1,5 @@
-import { useRouter } from "next/router";
 import React from "react";
-import Layout from "@/components/layouts/profLayout";
-import { useSession } from "next-auth/react";
+import Layout from "@/components/layouts/ProfLayout";
 import { trpc } from "@/utils/trpc";
 import Card from "@/components/card";
 import { getSession } from "next-auth/react";
@@ -40,20 +38,18 @@ function UserPage({
       <div className="  sm:h-[80vh] h-full flex  bg-vercel-900 sm:rounded-3xl sm:shadow-2xl sm:bg-gray-800 sm:bg-opacity-20  text-xl text-emerald-400">
         <div className="overflow-y-scroll h-full w-full flex flex-col ">
           <div className="pl-6 py-3 text-3xl border-b border-vercel-600 justify-center">
-            My Ideas
+            {isMe && <div>My Ideas</div>}
+            {!isMe && <div>Ideas</div>}
           </div>
-          <div className=" px-5 sm:px-10 gap-10  md:grid lg:grid-cols-2 xl:grid-cols-3  overflow-y-scroll">
+          <div className=" px-5 sm:px-10 md:gap-10  lg:grid lg:grid-cols-2 xl:grid-cols-3  overflow-y-scroll">
             {!ideas ||
               (ideas && ideas.length == 0 && (
-                <div className="text-xl pt-14">No Ideas</div>
+                <div className="text-xl pt-7">No Ideas</div>
               ))}
             {ideas &&
               ideas.length > 0 &&
               ideas.map((idea) => (
-                <div
-                  key={idea.file}
-                  className=" pt-7 last:pb-7 sm:pt-14 sm:last:pb-14"
-                >
+                <div key={idea.file} className=" pt-7 last:pb-7 lg:last:pb-0">
                   <Card idea={idea} />
                 </div>
               ))}
