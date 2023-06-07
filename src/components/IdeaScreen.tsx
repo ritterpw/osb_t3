fc043e8d51f9e0a80c6165db01f5fed9c71acc4a
paul Ritter <ritterpw@dukes.jmu.edu>
2023-06-07 01:02:56 -0400

    Update IdeaScreen.tsx
    refactor idea screen

--- src/components/IdeaScreen.tsx
+++ src/components/IdeaScreen.tsx
@@ -1,15 +1,11 @@
-import {
-  PauseIcon,
-  PlayIcon,
-  UserCircleIcon,
-} from "@heroicons/react/24/outline";
+import { UserCircleIcon } from "@heroicons/react/24/outline";
 import { Contribution, User } from "@prisma/client";
 import { useRouter } from "next/router";
-import React, { useContext } from "react";
+import React from "react";
 import MusicPlayer from "./MusicPlayer";
-import { AudioContext } from "@/context/audioContext";
 import { ideasWithLikes } from "types/ideasWithLikes";
 import { useSession } from "next-auth/react";
+import ReviewContributions from "./contributions/ReviewContributions";
 import Image from "next/image";
 
 interface IdeaScreenProps {
@@ -43,6 +39,8 @@ function IdeaScreen({
                   <Image
                     className=" h-20 w-20 rounded-full cursor-pointer hover:opacity-80  "
                     referrerPolicy="no-referrer"
+                    height={1000}
+                    width={1000}
                     src={user?.image}
                   />
                 )}
@@ -56,11 +54,7 @@ function IdeaScreen({
         </div>
         <div className="px-6">
           <div className=" py-3 md:py-6">{idea.description}</div>
-          <div
-            className={`rounded border border-vercel-600 ${
-              idea.userId === session?.user.id ? "mb-6" : ""
-            } `}
-          >
+          <div className={`rounded border border-vercel-600 `}>
             <MusicPlayer file={idea.file} idea={idea} />
           </div>
 
@@ -76,84 +70,30 @@ function IdeaScreen({
               </button>
             </div>
           )}
+          {idea.userId == session?.user.id && (
+            <div className="items-end text-right justify-end content-end ">
+              <button
+                onClick={() => {
+                  router.push(`/ideas/${idea.id}/contributeToIdea`);
+                }}
+                className=" py-2 px-4  my-6 rounded border border-vercel-700 shadow-md shadow-vercel-900 transition-all duration-300 ease-in hover:rounded-xl hover:font-bold hover:bg-emerald-500 hover:text-vercel-1000"
+              >
+                contribute to your idea
+              </button>
+            </div>
+          )}
         </div>
       </div>
 
       {contributions && contributions.length > 0 && (
-        <div className="rounded-b border-b md:border-x border-vercel-600 bg-vercel-1000 mx-auto shadow-lg  w-full md:w-[50%] overflow-y-scroll">
-          <div className="bg-vercel-1000 sticky top-0 py-2 text-xl lg:text-2xl  text-left border-b border-vercel-600">
-            <h1 className="font-bold pl-6">Pending Contributions</h1>
-          </div>
-          {contributions.map((c) => {
-            return <ContributionTab c={c} user={contributionUser} key={c.id} />;
-          })}
-        </div>
+        <ReviewContributions
+          contributions={contributions}
+          contributionUser={contributionUser}
+          ideaID={idea.id}
+        />
       )}
     </div>
   );
 }
 
-const ContributionTab = ({ c, user }: { c: Contribution; user: User }) => {
-  const { pauseAudio, playAudio } = useContext(AudioContext);
-
-  const this_audio: HTMLAudioElement | null = new Audio(c.file);
-
-  function handlePlay() {
-    if (this_audio) {
-      playAudio(this_audio);
-    }
-  }
-
-  function handlePause() {
-    pauseAudio();
-  }
-
-  return (
-    <div
-      key={c.id}
-      className=" w-full last:border-b-0  p-5  border-b border-vercel-600 text-xl  text-vercel-400   "
-    >
-      <div className="flex flex-col  w-full">
-        <div className="  w-full flex flex-row justify-between">
-          <div className=" my-auto grid grid-flow-col text-left space-x-2 ">
-            <button className="  h-8 w-8  shadow-md  bg-emerald-600 rounded-full text-gray-900 items-center justify-center text-center">
-              {user && user?.image != null && (
-                <Image
-                  className=" h-20 w-20 rounded-full cursor-pointer hover:opacity-80  "
-                  referrerPolicy="no-referrer"
-                  src={user?.image}
-                />
-              )}
-              {!user?.image && <UserCircleIcon className=" p-6" />}
-            </button>
-            <div className=" my-auto text-md xl:text-lg">
-              {user?.producer_name != null ? user?.producer_name : user?.name}
-            </div>
-          </div>
-        </div>
-        <div className="py-4">
-          <h1>{c.description}</h1>
-        </div>
-        <div>
-          <button
-            onClick={() => {
-              handlePlay();
-            }}
-            className="h-9 w-9 mr-6  border border-vercel-600  bg-vercel-1000 text-emerald-500  shadow-md rounded items-center justify-center text-center hover:bg-emerald-500 hover:text-gray-800 transition-all ease-in duration-200"
-          >
-            <PlayIcon className=" h-6 w-6  m-auto " />
-          </button>
-          <button
-            onClick={() => {
-              handlePause();
-            }}
-            className="h-9 w-9 mr-2  border border-vercel-600 bg-vercel-1000 text-emerald-500  shadow-md rounded items-center justify-center text-center hover:bg-emerald-500 hover:text-gray-800 transition-all ease-in duration-200"
-          >
-            <PauseIcon className=" ml-[1.5] h-6 w-6  m-auto " />
-          </button>
-        </div>
-      </div>
-    </div>
-  );
-};
 export default IdeaScreen;
