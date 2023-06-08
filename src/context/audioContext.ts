a9396ddef52dc92cad1722301f8aff25694bbb44
paul Ritter <ritterpw@dukes.jmu.edu>
2023-06-08 03:04:20 -0400

    Update audioContext.ts
    update audioContext types

--- src/context/audioContext.ts
+++ src/context/audioContext.ts
@@ -1,14 +1,26 @@
+import { playAudioInterface } from "@/pages/_app";
+import { User } from "@prisma/client";
 import React, { Dispatch, SetStateAction } from "react";
+
+export type AudioDetails = {
+  artist: User | undefined | null;
+  title?: string;
+  ideaID?: string;
+};
 
 export interface AudioContextInterface {
   audio: HTMLAudioElement | null;
+  audio_details: AudioDetails | null;
   setAudio: Dispatch<SetStateAction<HTMLAudioElement | null>>;
-  playAudio: (audio: HTMLAudioElement) => void;
+  setAudioDetails: Dispatch<SetStateAction<AudioDetails | null>>;
+  playAudio: (audio: playAudioInterface) => void;
   pauseAudio: () => void;
 }
 
 export const AudioContext = React.createContext<AudioContextInterface>({
   audio: null,
+  audio_details: null,
+  setAudioDetails: () => undefined,
   setAudio: () => undefined,
   playAudio: () => undefined,
   pauseAudio: () => undefined,
