3d564169d5d728e4e2a454c346f1a06cf1c623a0
paul Ritter <ritterpw@dukes.jmu.edu>
2023-06-08 03:05:29 -0400

    Create IdeaScreen.tsx
    add idea screen to idea components folder

---
+++ src/components/ideas/IdeaScreen.tsx
@@ -0,0 +1,94 @@
+import { UserCircleIcon } from "@heroicons/react/24/outline";
+import { Contribution, User } from "@prisma/client";
+import { useRouter } from "next/router";
+import React, { useState } from "react";
+import MusicPlayer from "../MusicPlayer";
+import { ideasWithLikes } from "types/ideasWithLikes";
+import { useSession } from "next-auth/react";
+import ReviewContributions from "../contributions/ReviewContributions";
+import Image from "next/image";
+import ContributeToIdea from "./ContributeToIdea";
+
+interface IdeaScreenProps {
+  idea: ideasWithLikes;
+  contributions?: Contribution[];
+  contributionUser: User;
+  user: User;
+}
+
+function IdeaScreen({
+  idea,
+  contributions,
+  contributionUser,
+  user,
+}: IdeaScreenProps) {
+  const router = useRouter();
+
+  const { data: session } = useSession();
+
+  const [openContributionModal, setOpenContributionModal] =
+    useState<boolean>(false);
+
+  return (
+    <div className="flex flex-col  overflow-y-clip  h-full w-screen bg-vercel-900 md:py-16 items-center">
+      <div className="px-3 rounded-t border-b md:border border-vercel-600 bg-gradient-to-b from-vercel-900 via-vercel-900 to-vercel-1000 w-[100%]  shadow-lg md:w-[50%]  flex flex-col">
+        <div className="  w-full px-6 pt-3 md:pt-6">
+          <div className="  w-full flex flex-row justify-between">
+            <div className="   text-xl lg:text-2xl  xl:text-3xl">
+              {idea.title}
+            </div>
+            <div className=" my-auto grid grid-flow-col text-left space-x-2 ">
+              <button className="  h-8 w-8  shadow-md  bg-emerald-600 rounded-full text-gray-900 items-center justify-center text-center">
+                {user && user?.image != null && (
+                  <Image
+                    className=" h-20 w-20 rounded-full cursor-pointer hover:opacity-80  "
+                    referrerPolicy="no-referrer"
+                    height={1000}
+                    width={1000}
+                    src={user?.image}
+                  />
+                )}
+                {!idea && <UserCircleIcon className=" p-6" />}
+              </button>
+              <div className=" my-auto text-md xl:text-lg">
+                {user?.producer_name != null ? user?.producer_name : user?.name}
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="px-6">
+          <div className=" py-3 md:py-6">{idea.description}</div>
+          <div className={`rounded border border-vercel-600 `}>
+            <MusicPlayer file={idea.file} idea={idea} user={user} />
+          </div>
+
+          <div className="items-end text-right justify-end content-end ">
+            <button
+              onClick={() => {
+                setOpenContributionModal(!openContributionModal);
+              }}
+              className=" py-2 px-4  my-6 rounded border border-vercel-700 shadow-md shadow-vercel-900 transition-all duration-300 ease-in hover:rounded-xl hover:font-bold hover:bg-emerald-500 hover:text-vercel-1000"
+            >
+              {idea.userId == session?.user.id
+                ? "Contribute To Your Idea"
+                : "Contribute To Idea"}
+            </button>
+          </div>
+        </div>
+      </div>
+      {openContributionModal && (
+        <ContributeToIdea setOpenContributionModal={setOpenContributionModal} />
+      )}
+
+      {contributions && contributions.length > 0 && (
+        <ReviewContributions
+          contributions={contributions}
+          contributionUser={contributionUser}
+          ideaID={idea.id}
+        />
+      )}
+    </div>
+  );
+}
+
+export default IdeaScreen;
