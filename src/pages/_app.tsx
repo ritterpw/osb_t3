e7b649a89dd7eb9cd18680021d44cc411e5e1d94
paul Ritter <ritterpw@dukes.jmu.edu>
2023-06-08 03:03:55 -0400

    Update _app.tsx
    update data for audio and add audio footer to component tree

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,5 @@
 // src/pages/_app.tsx
-import { AudioContext } from "@/context/audioContext";
+import { AudioContext, AudioDetails } from "@/context/audioContext";
 import { httpBatchLink } from "@trpc/client/links/httpBatchLink";
 import { loggerLink } from "@trpc/client/links/loggerLink";
 import { withTRPC } from "@trpc/next";
@@ -9,24 +9,37 @@ import { useContext, useEffect, useState } from "react";
 import superjson from "superjson";
 import type { AppRouter } from "../server/router";
 import "../styles/globals.css";
+import AudioFooter from "@/components/audio/AudioFooter";
+
+export interface playAudioInterface extends AudioDetails {
+  this_audio: HTMLAudioElement;
+}
 
 const MyApp: AppType = ({ Component, pageProps }) => {
   const [audio, setAudio] = useState<HTMLAudioElement | null>(null);
+  const [audio_details, setAudioDetails] = useState<AudioDetails | null>(null);
 
   const pauseAudio = () => {
     if (audio && !audio.paused) {
       audio.pause();
       setAudio(null);
+      setAudioDetails(null);
     }
   };
 
-  const playAudio = (this_audio: HTMLAudioElement) => {
+  const playAudio = ({
+    this_audio,
+    artist,
+    title,
+    ideaID,
+  }: playAudioInterface) => {
     if (audio !== this_audio) {
       pauseAudio();
     }
 
     this_audio.play();
     setAudio(this_audio);
+    setAudioDetails({ artist, title, ideaID });
 
     return;
   };
@@ -37,8 +50,18 @@ const MyApp: AppType = ({ Component, pageProps }) => {
       // you have a short session maxAge time. Shown here with default values.
       session={pageProps.session}
     >
-      <AudioContext.Provider value={{ audio, setAudio, playAudio, pauseAudio }}>
+      <AudioContext.Provider
+        value={{
+          audio,
+          setAudio,
+          audio_details,
+          setAudioDetails,
+          playAudio,
+          pauseAudio,
+        }}
+      >
         <Component {...pageProps} />
+        <AudioFooter />
       </AudioContext.Provider>
     </SessionProvider>
   );
