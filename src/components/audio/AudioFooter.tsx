8213dc42a3f5712299a9aa07cfa5eb5ea18fb77e
paul Ritter <ritterpw@dukes.jmu.edu>
2024-10-01 18:09:36 -0400

    audio footer update bottom position

--- src/components/audio/AudioFooter.tsx
+++ src/components/audio/AudioFooter.tsx
@@ -15,7 +15,7 @@ const AudioFooter = () => {
 
   return (
     <div
-      className="absolute bottom-0  right-0 bg-vercel-900 text-left py-2 px-4 rounded-lg rounded-br-none transition-all animate-fade-in duration-300  ease-in-out  flex flex-col "
+      className="fixed bottom-0 right-0  bg-vercel-900 text-left py-2 px-4 rounded-lg rounded-br-none transition-all animate-fade-in duration-300 ease-in-out flex flex-col"
       onClick={() => router.push(`/ideas/${audio_details?.ideaID}`)}
     >
       <div className="flex flex-row justify-between">
